1064cd9e2dbe81cd4174b9cca46d3c7a8d1ea018
aciderix <121115002+aciderix@users.noreply.github.com>
2026-02-24 21:39:24 +0100

    Add proto/launcher/DragonSoulLauncher.java

---
+++ proto/launcher/DragonSoulLauncher.java
@@ -0,0 +1,8 @@
+import com.perblue.rpg.RPGMain;
+
+public class DragonSoulLauncher {
+    public static void main(String[] args) {
+        System.out.println("DragonSoul Web Prototype");
+        RPGMain game = new RPGMain(null);
+    }
+}
